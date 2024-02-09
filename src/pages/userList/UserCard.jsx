48885d06055e49b38d06909e66a86270752fc8d4
muhammadali246397 <muhammadali246397@gmail.com>
2024-02-10 01:21:29 +0600

    make user card with information

--- src/pages/userList/UserCard.jsx
+++ src/pages/userList/UserCard.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const UserCard = () => {
+const UserCard = ({user}) => {
+    console.log(user)
+    const {firstName, lastName, image, email,address,company} = user;
+    console.log(user.image)
     return (
-        <div>
-            this is user UserCard
+        <div className=''>
+            <div className='md: h-[400px] bg-slate-50 border rounded-lg text-center shadow-xl hover:shadow-2xl'>
+                <div className='w-full bg-[#8bdaff] h-[35%] relative rounded-md'>
+                    <div className=' absolute -bottom-14 left-1/3 right-1/3'>
+                        <img className='w-[120px] h-[120px] mx-auto object-cover rounded-full border-2 border-orange-800 p-2' src={image} alt="" />
+                    </div>
+                </div>
+                <div>
+                    <h3 className='font-semibold mt-16 text-2xl'>{firstName} { lastName}</h3>
+                    <p>{email}</p>
+                    <p className='font-semibold'>Adress</p>
+                    <p>{address?.address}</p>
+                    <p className='font-semibold'>Company</p>
+                    <p>{company?.name}</p>
+                </div>
+            </div>
+            
         </div>
     );
 };
--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import UserCard from './UserCard';
 
 const UserList = () => {
-    const [user, setUser] = useState('')
-    console.log(user)
+    const [users, setUsers] = useState()
       useEffect(() => {
         fetch("https://dummyjson.com/users")
         .then(res => res.json())
-        .then(data => setUser(data))
+        .then(data => setUsers(data.users))
       }
       
       ,[])
     return (
-        <div className='container mx-auto'>
-            <div className=' flex justify-around border-b bg-[#f1f1f1] py-4'>
+        <div className='container mx-auto px-4'>
+            <div className=' flex justify-between md:justify-around border-b bg-[#f1f1f1] py-4'>
                 <div>
-                    <input className='max-w-xs px-5 py-3 border rounded-md' type="text" placeholder='Sarch User' />
-                    <button className='btn bg-[#0ea5e9] hover:bg-[#0284c7] focus:bg-[#0284c7] text-white ml-4'>Sarch</button>
+                    <input className=' px-5 py-3 border rounded-md' type="text" placeholder='Sarch User' />
+                    <button className='btn bg-[#0ea5e9] hover:bg-[#0284c7] focus:bg-[#0284c7] text-white md:ml-4'>Sarch</button>
                 </div>
                 <div>
                     <select className="select select-bordered">
@@ -27,9 +27,17 @@ const UserList = () => {
                     </select>
                 </div>
             </div>
-            <div className='mt-10'>
-                <h1 className='text-6xl text-center font-bold'><span className='border-b-4 border-[#0ea5e9]'>Our all users</span></h1>
+            <div className='my-10 pb-10'>
+                <h1 className='text-6xl text-center font-bold'><span className='border-b-4 border-[#0ea5e9]'>All users</span></h1>
 
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+               {
+                users?.map((user,index) => <UserCard
+                key={index}
+                user={user}
+                ></UserCard>)
+               }
             </div>
         </div>
     );
